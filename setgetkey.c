4c9b959eb19306be5bca31587a66941347d91d72
julianny <julianny.favinha@gmail.com>
2016-11-27 17:15:35 -0200

    Fixed some bugs

--- setgetkey.c
+++ setgetkey.c
@@ -20,29 +20,33 @@
 
 #include <linux/unistd.h>
 #include <linux/linkage.h>
-#include <linux/slab.h> // Biblioteca para kmalloc e kfree
+#include <linux/slab.h> // kmalloc e kfree
 #include <linux/jiffies.h> // Constantes jiffies e HZ
+#include <linux/uaccess.h> // strncpy_from_user e copy_to_user
+
+#define COUNT_ELEM_HASH 20 // qtd máxima de elementos na tabela hash **************************** PODE SER QUALQUER NUMERO!!! *********************
 
 typedef struct node {
-	char * value;
+	char *value;
 	int key; 
 	unsigned int lifeT; // Total Lifespan
 	unsigned int lifeS; // Inicio Lifespan
 	struct node *next;
-} Node, * ImplNode;
+} Node, *ImplNode;
 
 typedef struct hash {
 	struct node *head;
-} Hash, * ImplHash;
+} Hash, *ImplHash;
 
-int eleCount = 0; // ? Pode ser qualquer número, pessoalmente colocaria 20
+int eleCount = 0; // qtd atual de elementos na tabela hash
 ImplHash hashTable = NULL; // (ImplHash) kmalloc(sizeof(elecount * Hash))
 //TODO: Persistência?
 
-ImplNode createNode(int key, char *value, int lifespan){
+ImplNode createNode(int key, char *value, int lifespan) {
 	ImplNode newNode;
 
-	newNode = (ImplNode) kmalloc(sizeof(Node));
+	newNode = (ImplNode) kmalloc(sizeof(Node), GFP_KERNEL); // TODO: NULL? void * kmalloc(size_t size, int flags)
+	/* GFP_KERNEL: This is a normal allocation and might block. This is the flag to use in process context code when it is safe to sleep. The kernel will do whatever it has to in order to obtain the memory requested by the caller. This flag should be your first choice. */
 	newNode->key = key;
 	strcpy(newNode->value, value);
 	newNode->lifeT = lifespan;
@@ -51,59 +55,79 @@ ImplNode createNode(int key, char *value, int lifespan){
 	return newNode;
 }
 
-// TODO: Quando será falso?
+// TODO: Quando será falso?  ------ R: Quando o hash nao estiver cheio, ou seja, nao ultrapassou COUNT_ELEM_HASH
+// TODO: Em algum caso que não de pra inserir, é preciso olhar se o lifespan expirou?
 int insertToHash(int key, char *value, int lifespan){
-	int hashIndex = key % eleCount;
-	ImplNode newNode = createNode(key, value, lifespan);
-
-	if(!hashTable[hashIndex].head) {
-		hashTable[hashIndex].head = newNode;
+	int hashIndex = key % COUNT_ELEM_HASH;
+	ImplNode newNode;
+	
+	// É possível inserir somente se a qtd de elementos atual não ultrapassou o limite
+	if (eleCount < COUNT_ELEM_HASH) {
+		newNode = createNode(key, value, lifespan);
+		
+		// Incrementa qtd atual de elementos da tabela hash
+		eleCount++;
+		
+		if(!hashTable[hashIndex].head) {
+			hashTable[hashIndex].head = newNode;
+		}
+		else {
+			newNode->next = hashTable[hashIndex].head;
+			hashTable[hashIndex].head = newNode;
+		}
 		return 0; // Inseriu 
 	}
 
-	newNode->next = (hashTable[Index].head)
-	hashTable[index].head = newNode;
-	return 0; // Inseriu 
+	// Não conseguiu inserir 
+	return -1;
 }
 
-int deleteFromHash(int key){
+int deleteFromHash(int key) {
 	int hashIndex = key % eleCount;
 	ImplNode aux, myNode = hashTable[hashIndex].head;
 
-	if(!myNode){
-		return -1; // Não existe
-	}
-
-	aux = myNode;
-	while(myNode != NULL){
-		if(myNode->key == key){
-			if(myNode == hashTable[hashIndex].head)
-				hashTable[hashIndex].head = myNode.next;
-			else
-				aux->next = myNode->next;
-			
-			kfree(myNode);
-			return 0; // Removeu 
+	// É possível deletar somente se existir pelo menos um elemento
+	if (eleCount > 0) {
+		if (!myNode) {
+			return -1; // Não existe
 		}
+
 		aux = myNode;
-		myNode = myNode->next;
+		while (myNode != NULL) {
+			if (myNode->key == key) {
+				if (myNode == hashTable[hashIndex].head) {
+					hashTable[hashIndex].head = myNode->next;
+				}
+				else {
+					aux->next = myNode->next;
+				}
+				
+				kfree(myNode);
+				
+				// decrementa qtd atual de elementos da tabela hash
+				eleCount--;
+				return 0; // Removeu 
+			}
+			aux = myNode;
+			myNode = myNode->next;
+		}
 	}
 
 	return -1; // Não existe
 }
 
-char * getValue(int key){
+char *getValue(int key) {
 	int hashIndex = key % eleCount;
 	ImplNode myNode = hashTable[hashIndex].head;
-	char * value = NULL
+	char * value = NULL;
 
-	if(!myNode){
+	if (!myNode) {
 		return value; // Não existe
 	}
 
-	while(myNode != NULL){
-		if(myNode->key == key) {
-			if(jiffies/HZ - myNode->lifeS >= myNode->lifeT)
+	while (myNode != NULL) {
+		if (myNode->key == key) {
+			if (jiffies/HZ - myNode->lifeS >= myNode->lifeT)
 				deleteFromHash(myNode->key); // Tempo esgotado
 			else 
 				value = myNode->value; // Existe
@@ -115,18 +139,26 @@ char * getValue(int key){
 	return value; // Não existe
 }
 
-asmlinkage long sys_settmpkey(int key, char* value, unsigned int lifespan){
-	char * valueK;
+asmlinkage long sys_settmpkey(int key, char* value, unsigned int lifespan) {
+	char *valueK = NULL;
 
-	strncpy_from_user(valueK, value, strnlen_user( value, 10));
-	return insertToHash(key, valueK, lifespan);
+	// strncpy_from_user retorna zero em caso de sucesso, diferente de zero em caso de falha
+	if (!strncpy_from_user(valueK, value, strnlen_user(value, 10)))
+		return insertToHash(key, valueK, lifespan);
+		
+	return -1;
 }
 
-asmlinkage long sys_gettmpkey(int key, int n, char* value){
+asmlinkage long sys_gettmpkey(int key, int n, char* value) {
 	char * valueK = getValue(key);
+	
 	if (!valueK)
 		return -1;
-	copy_to_user(value, valueK, n);
+	
+	// copy_to_user retorna zero em caso de sucesso, ou diferente de zero para indicar o número de bytes que não foram transferidos	
+	if (!copy_to_user(value, valueK, n))
+		return -1;
+		
 	return 0;
 }
 
